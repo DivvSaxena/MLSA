36d4b6cf8db18ab3e5bc7070cacdc3a34531d631
DivvSaxena <saxenadivv@gmail.com>
2024-04-11 14:14:15 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/bg2.png";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "nature";
 
 /**
  * Sort description that expands on your title on the Home component.
@@ -31,11 +31,11 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web Developer",
   "User experience",
   "Inclusive design",
   "Focus group testing",
-  "Mobile user interfaces",
+  "Mobile First Design",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Breathing New Life into Your Websites";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/bg3.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "nature";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "pacman game",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "pacman game built with javascript",
+    url: "https://pacman-divvsaxena.vercel.app/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "My Blogs at Hashnode",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Best Technical Blogs out there!",
+    url: "https://divvsaxena.hashnode.dev/",
   },
   {
-    title: "My Resume Site",
+    title: "Spotify Clone",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Do you wanna listen to music here's the site for you!",
+    url: "https://spotify-clone-nine-tan.vercel.app/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Space Invader Game",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Get ready to defend the galaxy shoot the enemies now!",
+    url: "https://spaceinvasion-divvsaxena.vercel.app/",
   },
 ];
 
